a074cff3a9e291df01fbdbfdfb334316ef1398d3
AustinIsom <Austin.Isom@live.com>
2021-10-26 21:08:46 -0400

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,10 +1,10 @@
 function buttonPress() {
         let pressed = 0;
         switch (pressed) {
-            case 0: document.getElementById("buttonTest").innerHTML = "I am unchanged.";
+            case 0: document.getElementById("buttonTest").innerHTML = "I am unchanged.",
                 pressed = 1;
                 break;
-            case 1: document.getElementById("buttonTest").innerHTML = "You done it!";
+            case 1: document.getElementById("buttonTest").innerHTML = "You done it!",
                 pressed = 0;
                 break; 
             default: document.getElementById("buttonTest").innerHTML = "What happened?";
