c3a51798b4de096c0b669f5da3e6329ae051849c
AustinIsom <Austin.Isom@live.com>
2021-10-26 20:58:57 -0400

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,14 +1,14 @@
-function checkButtonState = checkButtonState => {
-    let buttonPressed = 0;
-    switch (button) {
-        case 0: document.getElementById("buttonTest").innerHTML = "I am unchanged.";
-            buttonPressed = 1;
-            break;
-        case 1: document.getElementById("buttonTest").innerHTML = "You done it!";
-            buttonPressed = 0;
-            break; 
-        default: document.getElementById("buttonTest").innerHTML = "What happened?";
-            break;
-    } 
-    
-}
+function buttonPress() {
+        const pressed = 0;
+        switch (pressed) {
+            case 0: document.getElementById("buttonTest").innerHTML = "I am unchanged.";
+                pressed = 1;
+                break;
+            case 1: document.getElementById("buttonTest").innerHTML = "You done it!";
+                pressed = 0;
+                break; 
+            default: document.getElementById("buttonTest").innerHTML = "What happened?";
+                break;
+        
+    }
+};
