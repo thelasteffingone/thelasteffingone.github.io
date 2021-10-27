23a056e4ce8c428cee116f74c0dd3fb48b372a92
AustinIsom <Austin.Isom@live.com>
2021-10-26 20:46:59 -0400

    js testing

--- js/test.js
+++ js/test.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+let checkButtonState = () => {
+    let buttonPressed = 0;
+    switch (button) {
+        case 0: document.getElementById("buttonTest").innerHTML = "I am unchanged.";
+            buttonPressed = 1;
+            break;
+        case 1: document.getElementById("buttonTest").innerHTML = "You done it!";
+            buttonPressed = 0;
+            break; 
+        default: document.getElementById("buttonTest").innerHTML = "What happened?";
+            break;
+    } 
+    
+}
