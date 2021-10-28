9c81cea5a4e88a89cf9d95df8744e237a171b96d
thelasteffingone <Austin.Isom@live.com>
2021-10-28 01:33:06 -0400

    php test

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
+echo "Hi, there. Care to tell me your name?";
+$username readline(">>");
 
-$playerName = Austin;
-$armor = "None";
-$weapon = "None";
-
-class player(){
-    $name = $playerName;
-    $playerGold = "Gold: " . 100;
-    $playerWeapon = 'Weapon: ' . $weapon;
-    $armor = 'Armor: ' . $armor;
+function show_user_name($username) {
+   while($username){
+     echo "Username: $username";
+  };
 };
 
-function getGold($amount){
-    return $playerGold += $amount;
-    
 };
-
-getGold(35);
-getGold(90);
-
-echo player();
 ?>
