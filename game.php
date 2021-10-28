f83796760537137cc568e6df92d7f360bb094046
thelasteffingone <Austin.Isom@live.com>
2021-10-28 01:44:08 -0400

    Update game.php

--- game.php
+++ game.php
@@ -1,12 +1,4 @@
 <?php
-echo "Hi, there. Care to tell me your name?";
-$_SET readline(">>");
-
-function show_user_name($_SET) {
-   while($_SET){
-     echo "Username: $_SET";
-  };
-};
-
-};
+ echo "Hello, Internet Traveler. Do you have a name?\n";
+ $player_name = readline(">>");
 ?>
